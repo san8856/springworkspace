06391bc25aed84761ab2d46edbb6234d04395cbd
san8856 <qkqkq9948@naver.com>
2025-07-03 17:51:57 +0900

    0703

--- sp02/src/main/java/com/yedam/app/common/web/ParameterController.java
+++ sp02/src/main/java/com/yedam/app/common/web/ParameterController.java
@@ -1,11 +1,19 @@
 package com.yedam.app.common.web;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yedam.app.emp.service.EmpVO;
+
+import lombok.NoArgsConstructor;
 
 @CrossOrigin(origins = "*") //cors 설정
 @Controller
@@ -14,7 +22,7 @@ public class ParameterController {
 	// Content-type : application/x-www-form-urlencoded
 	// Method : 상관없음
 	
-	// QueryString, 1) 커맨드객체 (어노테이션을 사용하지 않는 방식, 객체)
+	// QueryString, 1) 커맨드객체 (= 어노테이션을 사용하지 않는 방식, 데이터타입이 객체로 선언되는 경우)
 	@RequestMapping("comobj")
 	@ResponseBody //AJAX
 	public String connamdObject(EmpVO empVO) {
@@ -24,4 +32,77 @@ public class ParameterController {
 		result += "\t last_name : " + empVO.getLastName();
 		return result;
 	}
+	// QueryString, 2) @RequestParam (기본타입, 기본값)
+	@RequestMapping("reqparm")
+	@ResponseBody
+	public String requestParam
+	(@RequestParam Integer employeeId,// 필수
+					String lastName) {
+		String result = "";
+		result += "Path : /reqparm \n";
+		result += "\t employee_id : " + employeeId;
+		result += "\t last_name : " + lastName;
+		return result;
+	}
+	
+	// QueryString, 3) @PathVariable
+	// @PathVariable : 경로에 값을 포함하는 방식, 단일 값
+	// Method는 상관없음
+	// Content-type도 상관없음
+	@RequestMapping("path/{id}") // path/Hong, path/1234
+	@ResponseBody
+	public String pathvariable
+	(@PathVariable String id) {
+		String result = "";
+		result += "Path : /path/{id} \n";
+		result += "\t id : " + id;
+
+		return result;
+	}
+	
+	
+	// @RequestBody : JSON 포맷, 배열 or 객체
+	// Method : POST, PUT
+	// Content-type : application/json
+	@PostMapping("resbody")
+	@ResponseBody
+	public EmpVO resquestBody(@RequestBody EmpVO empVO)	{
+		return empVO;
+	}
+	
+	
+	
+	@PostMapping("resbodyList")
+	@ResponseBody
+	public List<EmpVO> resbodyList(@RequestBody List<EmpVO> list) {
+		return list;
+	}
+	//JSON parse error: Cannot deserialize(역직렬화) value of type `java.util.ArrayList<com.yedam.app.emp.service.EmpVO>` from Object
+	//받는쪽에서 반드시 배열을 받는다고 알려줘야함 [{객체}]
+	
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sp02/src/main/java/com/yedam/app/emp/web/EmpController.java
@@ -0,0 +1,96 @@
+package com.yedam.app.emp.web;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.yedam.app.emp.service.EmpService;
+import com.yedam.app.emp.service.EmpVO;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller 
+// DispatherServlet이 활용한 정보를 등록하는 Bean
+// route : 사용자의 요청(endpoint)와 그에 대한 처리
+// 	URI + HTTP METHOD (get post delete) => Service => Response(View or Data)
+
+@RequiredArgsConstructor
+public class EmpController {
+	// 해당 컨트롤러에서 제공하는 서비스 목록
+	//final이 들어가면 무조건 생성자 요청, RequiredArgsConstructor(lombok) 사용.
+	private final EmpService empService;
+	
+	// GET : 조회, 빈페이지, 데이터 조작(삭제)
+	// POST : 데이터 조작(등록, 수정)
+	
+	//전체조회 : GET
+	@GetMapping("empList")// URI + METHOD
+	public String empList(Model model) {
+		//2) 수행기능 => Service
+		List<EmpVO> list = empService.findAllList();
+		//2-1) view 에 전달할 데이터 담기
+		model.addAttribute("emps", list);
+		//3) 응답형태 : View
+		return "emp/list";
+		//classpath:/template/emp/list.html
+		//prefix              return   suffix
+	}
+	//단건조회 : GET => QueryString
+	@GetMapping("empInfo")
+	public String empInfo(EmpVO empVO, Model model) {
+		EmpVO findVO = empService.findInfoById(empVO);
+		model.addAttribute("emp", findVO);
+		return "emp/info";
+	}
+	// 등록 - 페이지 : GET
+	@GetMapping("empInsert")
+	public String empInsertForm() {
+		return "emp/insert";
+	}
+	
+	// 등록 - 처리 : POST => <form/> submit QueryString
+	@PostMapping("empInsert")
+	public String empInsertProcess(EmpVO empVO) {
+		int eid = empService.createInfo(empVO);
+		String url = null;
+		if(eid > -1 ) {
+			//정상적으로 등록
+			url = "redirect:empInfo?employeeId=" + eid;
+		}else {
+			//등록되지 않은 경우 -> 얘는 등록이 실패했을때 실질적으로 실행되지 않을것 - 등록 실패 = 에러 이기 때문에 코드가 중단될것임.
+			url="redirect:empList";
+		}
+		return "";
+	}
+	
+	// 수정 - 페이지 : GET <=> 단건조회와 같음. 경로, 이름 빼고 나머지는 다 같다고 보면 됌
+	@GetMapping("empUpdate")
+	public String empUpdateForm(EmpVO empVO, Model model) {
+		EmpVO findVO = empService.findInfoById(empVO);
+		model.addAttribute("emp", findVO);
+		return "emp/update";
+	}
+	
+	// 수정 - 처리 : POST + AJAX + JSON
+	@PostMapping("empUpdate")
+	@ResponseBody // AJAX => Model 사용하지 않음
+												//JSON / 보내주는 데이터가 JSON 만이 아닐수도 있음.
+	public Map<String, Object> empUpdateProcess(@RequestBody EmpVO empVO) {
+		return empService.modifyInfo(empVO);
+	}
+	
+	// 삭제 - 처리 : GET
+	@GetMapping("empDelete")
+	public String empDelete(Integer employeeId) { 
+		empService.removeInfo(employeeId);
+		return "redirect:empList"; //삭제된 데이터는 안보이게 돌려준다
+	}
+
+	
+}
