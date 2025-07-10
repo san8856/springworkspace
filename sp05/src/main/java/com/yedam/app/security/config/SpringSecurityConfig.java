bd64ab48c6907b2767022b5486e252fe771ff5de
san8856 <qkqkq9948@naver.com>
2025-07-10 17:56:24 +0900

    07.10

--- sp05/src/main/java/com/yedam/app/security/config/SpringSecurityConfig.java
+++ sp05/src/main/java/com/yedam/app/security/config/SpringSecurityConfig.java
@@ -2,25 +2,68 @@ package com.yedam.app.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import jakarta.servlet.DispatcherType;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
+
+//    private final SecurityFilterChain filterChain;
+//
+//    SpringSecurityConfig(SecurityFilterChain filterChain) {
+//        this.filterChain = filterChain;
+//    }
 	@Bean // 비밀번호 암호화 : 단방향
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();	
 	}
 	
+	//인증 및 인가 : Lambda DSL
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http //Security가 적용될 URI / 핵심
+			.authorizeHttpRequests( authrize
+					-> authrize
+					  .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+					  .requestMatchers("/","/all").permitAll()
+					  .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+					  .requestMatchers("/admin/**").hasAnyAuthority("ROLE_ADMIN")
+					  .anyRequest().authenticated())
+			.formLogin(formLogin // 필수
+					-> formLogin
+					.usernameParameter("id")
+					.defaultSuccessUrl("/all"))
+			.logout(logout   // 필수
+					-> logout
+					.logoutSuccessUrl("/all")
+					.invalidateHttpSession(true)
+					.deleteCookies("JSESSIONID")); 
+//		http.csrf(csrf -> csrf.disalble());
+		return http.build();
+	}
+	
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return web ->
+				web
+					.ignoring()
+					.requestMatchers("/images/**", "/js/**", "/css/**"); //예외대상
+	}
+/*
 	@Bean //메모리상 인증정보 등록 => 테스트 전용 방식
 	InMemoryUserDetailsManager inMemoryUserDetailsService() {
 		UserDetails user = User.builder()
-							   .username("user01")
+							   .username("user1")
 							   .password(passwordEncoder().encode("1234"))
 							   .roles("USER")
 							   .build();
@@ -33,4 +76,5 @@ public class SpringSecurityConfig {
 		
 		return new InMemoryUserDetailsManager(user, admin);
 	}
+ */
 }
